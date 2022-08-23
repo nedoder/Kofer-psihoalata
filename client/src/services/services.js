3164acdae054e484c11ce3ccc744cda5d908d2c9
nedoder <nedoder89@gmail.com>
2022-08-23 21:57:18 +0200

    dist changed

--- client/src/services/services.js
+++ client/src/services/services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: `http://localhost:3000/`,
+    baseURL: `https://koferpsihoalata.me/`,
     withCredentials: false, //true
     headers: {
         Accept: "application/json",
