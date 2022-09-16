a85ad7233df2572474f9d67c69fe78254fbfb45c
nedoder <nedoder89@gmail.com>
2022-09-16 07:11:51 +0200

    config update

--- server/config/config.js
+++ server/config/config.js
@@ -2,17 +2,43 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "config.env" });
 
 module.exports = {
-  development: {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DB,
-    host: process.env.HOST,
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+    development: {
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DB,
+        host: process.env.HOST,
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    },
+    test: {
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DB,
+        host: process.env.HOST,
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    },
+    production: {
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DB,
+        host: process.env.HOST,
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
     }
-  }
 }
